ba708d7dd6a13f39aa662b0ae463dd672cd242af
lavic98 <lavic98@macbook-pro-2.station>
2020-05-09 10:45:15 +0200

    Soluzione con solo 3 metodo per la creazione degli archi

--- src/main/java/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
+++ src/main/java/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
@@ -6,13 +6,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import it.polito.tdp.artsmia.model.Adiacenza;
 import it.polito.tdp.artsmia.model.ArtObject;
 
 public class ArtsmiaDAO {
 
-	public List<ArtObject> listObjects() {
-		
+	public void listObjects(Map<Integer, ArtObject> idMap) {
+
 		String sql = "SELECT * from objects";
 		List<ArtObject> result = new ArrayList<>();
 		Connection conn = DBConnect.getConnection();
@@ -21,14 +23,44 @@ public class ArtsmiaDAO {
 			PreparedStatement st = conn.prepareStatement(sql);
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
-
-				ArtObject artObj = new ArtObject(res.getInt("object_id"), res.getString("classification"), res.getString("continent"), 
-						res.getString("country"), res.getInt("curator_approved"), res.getString("dated"), res.getString("department"), 
-						res.getString("medium"), res.getString("nationality"), res.getString("object_name"), res.getInt("restricted"), 
-						res.getString("rights_type"), res.getString("role"), res.getString("room"), res.getString("style"), res.getString("title"));
-				
-				result.add(artObj);
+				int id = res.getInt("object_id");
+				if (!idMap.containsKey(id)) {
+					ArtObject artObj = new ArtObject(res.getInt("object_id"), res.getString("classification"),
+							res.getString("continent"), res.getString("country"), res.getInt("curator_approved"),
+							res.getString("dated"), res.getString("department"), res.getString("medium"),
+							res.getString("nationality"), res.getString("object_name"), res.getInt("restricted"),
+							res.getString("rights_type"), res.getString("role"), res.getString("room"),
+							res.getString("style"), res.getString("title"));
+					idMap.put(artObj.getId(), artObj);
+				}
 			}
+			conn.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+	}
+	
+	public List<Adiacenza> getAdiacenza(){
+		
+		String sql="Select eo1.object_id as obj1, eo2.object_id as obj2, count(*) as peso "+
+	               "from exhibition_objects as eo1,exhibition_objects as eo2 "+
+	 			   "where eo1.exhibition_id = eo2.exhibition_id "+
+	               "and eo1.object_id > eo2.object_id "+
+	 			   " group by eo1.object_id, eo2.object_id ";
+		
+		List<Adiacenza> result = new ArrayList<Adiacenza>();
+		
+		Connection conn = DBConnect.getConnection();
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			
+			while (res.next()) {
+				result.add(new Adiacenza(res.getInt("obj1"),res.getInt("obj2"), res.getInt("peso")));
+				}
+			
 			conn.close();
 			return result;
 			
@@ -36,6 +68,8 @@ public class ArtsmiaDAO {
 			e.printStackTrace();
 			return null;
 		}
+		
+	 			   
 	}
 	
 }
--- src/main/java/it/polito/tdp/artsmia/model/Model.java
+++ src/main/java/it/polito/tdp/artsmia/model/Model.java
@@ -1,5 +1,57 @@
 package it.polito.tdp.artsmia.model;
 
+import java.util.*;
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.artsmia.db.ArtsmiaDAO;
+
 public class Model {
 
+	private Graph<ArtObject,DefaultWeightedEdge> graph;
+	private Map<Integer, ArtObject> idMap;
+	
+	public Model() {
+	idMap=new HashMap<Integer,ArtObject>();
+		
+	}
+	
+	/**
+	 * Cancello ed ogni volta ricreo il grafo
+	 * risolve molti scazzi. 
+	 * 
+	 * Passo l'identity map al dao, così la riempie direttamente lui. 
+	 * Per non creare doppioni
+	 * 
+	 */
+	public void creaGrafo(){
+		this.graph=new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		
+		//Lo faccio fare direttamente al dao mettendoli in un idMap
+		ArtsmiaDAO dao=new ArtsmiaDAO();
+		dao.listObjects(idMap);
+		
+		//aggiungo veritici
+		Graphs.addAllVertices(this.graph, idMap.values());
+		
+		//aggiungo archi
+		
+		for(Adiacenza a: dao.getAdiacenza()) {
+			if(a.getPeso()>0) {
+				Graphs.addEdge(this.graph, idMap.get(a.getObj1()), idMap.get(a.getObj2()), a.getPeso());
+			}
+		}
+	}
+	
+	public int nVertici() {
+		return this.graph.vertexSet().size();
+	}
+	
+	public int nArchi() {
+		return this.graph.edgeSet().size();
+	}
+	
+	
 }
